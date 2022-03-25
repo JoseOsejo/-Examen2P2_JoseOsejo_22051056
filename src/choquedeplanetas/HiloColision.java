8859476222ac28ffc9d768c4cc037ec48066b471
Jose Carlos Osejo <jcoq2003@gmail.com>
2022-03-25 17:20:46 -0600

    intento de hilo

--- src/choquedeplanetas/HiloColision.java
+++ src/choquedeplanetas/HiloColision.java
@@ -5,10 +5,56 @@
  */
 package choquedeplanetas;
 
+import java.util.Random;
+import javax.swing.JOptionPane;
+import javax.swing.JProgressBar;
+
 /**
  *
  * @author jcoq2
  */
-public class HiloColision {
-    
+public class HiloColision extends Thread {
+
+    Random randomizer = new Random();
+
+    private Cientifico cientifico;
+    private boolean yaExiste;
+    private JProgressBar barraPlaneta;
+    private Planeta planeta1;
+    private Planeta planeta2;
+
+    public HiloColision(JProgressBar barraPlaneta, Planeta planeta1, Planeta planeta2, Cientifico cientifico) {
+        this.cientifico = cientifico;
+        yaExiste = true;
+        this.barraPlaneta = barraPlaneta;
+        this.planeta1 = planeta1;
+        this.planeta2 = planeta2;
+    }
+
+    @Override
+    public void run() {
+        while (yaExiste) {
+            barraPlaneta.setValue(barraPlaneta.getValue() + 1);
+            if (barraPlaneta.getValue() == barraPlaneta.getMaximum()) {
+                yaExiste = false;
+                JOptionPane.showMessageDialog(null, "Finalizo");
+                barraPlaneta.setValue(0);
+
+                int x = (planeta1.getX() + planeta2.getX()) / 2;
+                int y = (planeta1.getY() + planeta2.getY()) / 2;
+                int peso = (planeta1.getPeso() + planeta2.getPeso()) / 2;
+                int size = (planeta1.getTamanio()+ planeta2.getTamanio()) / 2;
+                int randomColision = randomizer.nextInt(100) + 0;
+                
+                
+
+            }
+
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException ex) {
+            }
+        }
+    }
+
 }
